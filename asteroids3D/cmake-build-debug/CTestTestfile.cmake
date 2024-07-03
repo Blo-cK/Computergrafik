d7230b9b2ead902559063c8cfa0525dc42fd7db3
Blo-cK <ro.bienert@web.de>
2024-07-03 14:04:15 +0200

    initial commit

---
+++ asteroids3D/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robert/Computergrafik/asteroids3D
+# Build directory: /home/robert/Computergrafik/asteroids3D/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ viewer/CMakeFiles/viewer.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/robert/Computergrafik/viewer/viewer.cc" "CMakeFiles/viewer.dir/viewer.cc.o" "gcc" "CMakeFiles/viewer.dir/viewer.cc.o.d"
+  "/home/robert/Computergrafik/viewer/wavefront.cc" "CMakeFiles/viewer.dir/wavefront.cc.o" "gcc" "CMakeFiles/viewer.dir/wavefront.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
